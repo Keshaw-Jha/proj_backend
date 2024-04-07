a655a49cd49c62c7e59ff5e87887f03ba0861283
Keshaw-Jha <78222085+Keshaw-Jha@users.noreply.github.com>
2024-04-07 21:52:13 +0530

    getStats added

---
+++ controllers/dashboardController.js
@@ -0,0 +1,58 @@
+const Form = require("../models/formModel");
+
+const getTickets = async (req, res) => {
+  try {
+    const ticketsRecords = await Form.find({});
+    res.status(200).json({
+      status: "success",
+      data: ticketsRecords,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "failed",
+      error: err,
+    });
+  }
+};
+
+const getDashboardStats = async (req, res) => {
+  try {
+    const ActiveUsers = await Form.countDocuments({
+      entryAt: { $exists: true },
+      $and: [{ exitAt: { $exists: false } }, { exitAt: "" }],
+    });
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+    const startOfToday = today;
+    const endOfToday = new Date(today);
+    endOfToday.setDate(endOfToday.getDate() + 1);
+    const TicketsBooked = await Form.countDocuments({
+      createdAt: {
+        $gte: startOfToday,
+        $lte: endOfToday,
+      },
+    });
+    const TotalExits = await Form.countDocuments({
+      exitAt: { $exists: true },
+      createdAt: {
+        $gte: startOfToday,
+        $lte: endOfToday,
+      },
+    });
+    res.status(200).json({
+      status: "success",
+      data: {
+        activeUsers: ActiveUsers,
+        ticketsBooked: TicketsBooked,
+        totalExits: TotalExits,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "failed",
+      error: err,
+    });
+  }
+};
+
+module.exports = { getTickets, getDashboardStats };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,11 +2,14 @@ const express = require("express");
 const form = require("../controllers/formController.js");
 const otp = require("../controllers/otpController.js");
 const qr = require("../controllers/qrController.js");
+const dashboard = require("../controllers/dashboardController.js");
 
 const router = express.Router();
 
 router.post("/submitForm", form.submitForm);
 router.post("/submitOtp", otp.verifyOtp);
 router.post("/getQr", qr.getQr);
+router.get("/getTickets", dashboard.getTickets);
+router.get("/getStats", dashboard.getDashboardStats);
 
 module.exports = router;
